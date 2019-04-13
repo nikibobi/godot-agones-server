e886e59bde3fa1aa3c7e342227dd4e24580a8b0a
Borislav Kosharov <bosakmaw@gmail.com>
2019-04-13 18:09:01 +0300

    add swagger ui

--- src/GodotAgones.Server/Startup.cs
+++ src/GodotAgones.Server/Startup.cs
@@ -56,6 +56,12 @@ namespace GodotAgones.Server
 
             app.UseSwagger();
 
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Master Server V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
         }
     }
